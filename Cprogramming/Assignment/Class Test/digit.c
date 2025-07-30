156c042747c2146cb463b2cb8eccf18595c1bc23
badakhnikita <144913895+badakhnikita@users.noreply.github.com>
2025-07-30 09:06:19 +0530

    Add files via upload

---
+++ Cprogramming/Assignment/Class Test/digit.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    int x=223410;
+    int q=x%100;
+    printf("%d",q);
+}
---
+++ Cprogramming/Assignment/Class Test/sec.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int hrs=3;
+    int min=120;
+    int sec=60;
+    int totalsec=(hrs*3600)+(min*60)+sec;
+    printf("%d",totalsec);
+}
